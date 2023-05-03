b64d8956c891c7535338ff4ae7888d922d8f7ca1
Dyan <lumbar_plaice0i@icloud.com>
2023-05-03 19:01:01 -0300

    chore: initial commit

---
+++ Pocket Money/Pocket_MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_MoneyApp.swift
+//  Pocket Money
+//
+//  Created by Dyan on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
