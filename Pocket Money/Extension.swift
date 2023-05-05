a8098b3d90e2bed54fc9990899d1330f58f77569
Dyan <lumbar_plaice0i@icloud.com>
2023-05-05 10:13:39 -0300

    feat: new color

---
+++ Pocket Money/Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Extension.swift
+//  Pocket Money
+//
+//  Created by Dyan on 5/4/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let gray7 = Color("gray7")
+}
+
