81061101321e43210eda89a938cb3ff43a8a2104
Dyan <lumbar_plaice0i@icloud.com>
2023-05-05 23:11:48 -0300

    feat: api auth

--- Pocket Money/SingIn.swift
+++ Pocket Money/SingIn.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 // MARK: - Model
-struct SingInAccount {
-    var email: String = ""
-    var password: String = ""
+struct SingInAccount: Codable {
+    var email: String = "olivier@mail.com"
+    var password: String = "bestPassw0rd"
+}
+
+struct User: Decodable {
+    let email: String
+    let firstName: String
+    let lastName: String
+    let age: Int
+    let id: Int
+    
+     enum CodingKeys: String, CodingKey {
+        case email
+        case firstName = "firstname"
+        case lastName = "lastname"
+        case age
+        case id
+    }
+}
+
+struct Auth: Decodable {
+    let accessToken: String
+    let user: User
+    
+    
+     enum CodingKeys: String, CodingKey {
+        case accessToken
+        case user
+    }
 }
 
 // MARK: - VIEWMODEL
@@ -20,6 +47,9 @@ struct SingInViewModel {
 
 struct SingIn: View {
     @State var viewModel = SingInViewModel()
+    
+    @State private var confirmationMessage: String = "Deseja tentar novamente"
+    @State private var showingConfirmation: Bool = false
     
     var body: some View {
         VStack{
@@ -55,15 +85,17 @@ struct SingIn: View {
             }
             .padding()
             
-            NavigationLink(destination: {
-                RegisteredChildren()
-            }, label: {
+            Button{
+                Task {
+                    await placeOrder()
+                }
+            } label: {
                 Text("Entrar")
                     .fontWeight(.semibold)
                     .foregroundColor(.white)
                     .frame(maxWidth: .maximum(300, 300))
                     .padding(4)
-            })
+            }
             .buttonStyle(.borderedProminent)
             .tint(.purple)
             .padding(.bottom)
@@ -72,8 +104,34 @@ struct SingIn: View {
         }
         .navigationTitle("Login")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Email ou senha incorreto", isPresented: $showingConfirmation){
+            Button("OK"){}
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(viewModel.singInAccount) else {
+            print("Failed to encode order")
+            return
+        }
+        guard let url = URL(string: "http://localhost:3000/login") else {return}
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedSingIN = try JSONDecoder().decode(Auth.self, from: data)
+            print("code \(decodedSingIN)")
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
+
+
 
 struct SingIn_Previews: PreviewProvider {
     static var previews: some View {
