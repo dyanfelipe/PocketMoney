1257ae242e1a9a92fa3f5905f6288f0e2ca80194
Dyan <lumbar_plaice0i@icloud.com>
2023-05-06 21:46:02 -0300

    feat: show list user

--- Pocket Money/RegisteredChildren.swift
+++ Pocket Money/RegisteredChildren.swift
@@ -15,38 +15,89 @@ struct NewChild {
     var showSheet: Bool = false
 }
 
-// MARK: - VIEWMODEL
-struct RegisteredChildrenViewModel {
-    var child = NewChild()
+struct Childs: Codable {
+    let id: Int
+    let email: String
+    let name: String
+    let savedValue: String
+    let amountToSpend: String
     
-    mutating func showSheetToggle() {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case email
+        case savedValue = "saved_value"
+        case amountToSpend = "amount_to_spend"
+    }
+}
+
+// MARK: - VIEWMODEL
+class RegisteredChildrenViewModel: ObservableObject {
+    var child = NewChild()
+    @Published var childs: [Childs] = []
+    
+     func showSheetToggle() {
         self.child.showSheet.toggle()
+    }
+    
+
+    
+    func getChilds() async {
+        guard let url = URL(string: "http://localhost:3000/childs") else {return}
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decodedResponse = try JSONDecoder().decode([Childs].self, from: data)
+            childs = decodedResponse
+        }catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func convetRealToCentsDecimal(savedValueD: String, amountToSpendD: String) -> String {
+        let savedValueD = Double(savedValueD) ?? 0.0
+        let amountToSpendD = Double(amountToSpendD) ?? 0.0
+        let sumDouble = ((savedValueD + amountToSpendD) / 100) * 100
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.locale = Locale(identifier: "pt_BR")
+        numberFormatter.minimumFractionDigits = 2
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: sumDouble)) ?? "0.0"
+        
     }
 }
 
 struct RegisteredChildren: View {
-    @State var viewModel = RegisteredChildrenViewModel()
+    @StateObject var viewModel = RegisteredChildrenViewModel()
     
     var body: some View {
         VStack{
             Form{
-                Section("Dyan Filipe Rdrigues da silva") {
-                    Text("Valor para gastar: R$ 120.00")
-                        .font(.subheadline)
-                        .foregroundColor(.gray7)
-                        .fontWeight(.medium)
-                        .listRowSeparator(.hidden)
-                    Text("Valor Guardado: R$ 270.00")
-                        .foregroundColor(.gray7)
-                        .font(.subheadline)
-                        .fontWeight(.medium)
-                    
-                    Text("Total: R$ 390.00")
-                        .foregroundColor(.gray7)
-                        .font(.title2)
-                        .fontWeight(.medium)
-                        .frame(maxWidth: .infinity, alignment: .trailing)
+                ForEach(viewModel.childs, id: \.id ) { child in
+                    let newValueFormatted = viewModel.convetRealToCentsDecimal(savedValueD: child.savedValue, amountToSpendD: child.amountToSpend)
+                    Section(child.name) {
+                        Text("Valor para gastar: R$ \(child.amountToSpend)")
+                            .font(.subheadline)
+                            .foregroundColor(.gray7)
+                            .fontWeight(.medium)
+                            .listRowSeparator(.hidden)
+                        Text("Valor Guardado: R$ \(child.savedValue)")
+                            .foregroundColor(.gray7)
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                        
+                        Text("Total: R$ \(newValueFormatted)")
+                            .foregroundColor(.gray7)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
                 }
+
+            }
+            .task {
+                await viewModel.getChilds()
             }
             .sheet(isPresented: $viewModel.child.showSheet) {
                 NavigationView {
