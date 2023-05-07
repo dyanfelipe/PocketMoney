ce240109ae192acf571c02f516bda594c4dd9a3b
RodrigoPoliveira <rodrigopoliveira1@gmail.com>
2023-05-07 18:19:32 -0300

    Ajustes HistoryView

--- Pocket Money/HistoryView.swift
+++ Pocket Money/HistoryView.swift
@@ -20,17 +20,17 @@ struct HistoryItemModel: Identifiable {
 //MARK - VIEW MODEL
 struct HistoryViewModel {
     let history = [
-        HistoryItemModel(amount: "- R$ 35,00", description: "Lanche", date: "05/05/2023", tag: "Gasto"),
-        HistoryItemModel(amount: "- R$ 12,00", description: "Sorvete", date: "01/05/2023", tag: "Gasto"),
-        HistoryItemModel(amount: "+ R$ 80,00", description: "", date: "20/04/2023", tag: "Guardado"),
-        HistoryItemModel(amount: "+ R$ 150,00", description: "Mesada", date: "20/04/2023", tag: "Depósito"),
-        HistoryItemModel(amount: "- R$ 18,00", description: "Uber", date: "13/04/2023", tag: "Gasto"),
-        HistoryItemModel(amount: "- R$ 5,00", description: "Chocolate", date: "13/04/2023", tag: "Gasto"),
-        HistoryItemModel(amount: "- R$ 31,00", description: "Presente", date: "12/04/2023", tag: "Gasto"),
-        HistoryItemModel(amount: "- R$ 4,40", description: "Ônibus", date: "10/04/2023", tag: "Gasto"),
-        HistoryItemModel(amount: "- R$ 20,00", description: "Cinema", date: "10/04/2023", tag: "Gasto"),
-        HistoryItemModel(amount: "- R$ 4,40", description: "Ônibus", date: "10/04/2023", tag: "Gasto"),
-        HistoryItemModel(amount: "- R$ 10,00", description: "Brinquedo", date: "05/04/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "-35,00", description: "Lanche", date: "05/05/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "-12,00", description: "Sorvete", date: "01/05/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "80,00", description: "", date: "20/04/2023", tag: "Guardado"),
+        HistoryItemModel(amount: "150,00", description: "Mesada", date: "20/04/2023", tag: "Depósito"),
+        HistoryItemModel(amount: "-18,00", description: "Uber", date: "13/04/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "-5,00", description: "Chocolate", date: "13/04/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "-31,00", description: "Presente", date: "12/04/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "-4,40", description: "Ônibus", date: "10/04/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "-20,00", description: "Cinema", date: "10/04/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "-4,40", description: "Ônibus", date: "10/04/2023", tag: "Gasto"),
+        HistoryItemModel(amount: "-10,00", description: "Brinquedo", date: "05/04/2023", tag: "Gasto"),
     ]
 }
 
@@ -95,7 +95,7 @@ struct History: View {
                 WalletHistoryItem(amount: historyItem.amount, description: historyItem.description, date: historyItem.date, tag: historyItem.tag)
             }
         }
-        .padding(EdgeInsets(top: -40, leading: 20, bottom: 0, trailing: 20))
+        .padding(EdgeInsets(top: -40, leading: 20, bottom: 25, trailing: 20))
         .scrollIndicators(.hidden)
     }
 }
