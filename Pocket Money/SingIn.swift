0f097d3a43132cd0357f8aa50e3b733d254040b6
Dyan <lumbar_plaice0i@icloud.com>
2023-05-08 14:58:55 -0300

    feat: forced navigation

--- Pocket Money/SingIn.swift
+++ Pocket Money/SingIn.swift
@@ -82,59 +82,57 @@ struct SingIn: View {
     
     var body: some View {
         VStack{
-            Image("happy.family")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 300, maxHeight: 300)
-                .padding([.bottom])
-            
-            HStack{
-                Image(systemName: "envelope.fill")
-                    .foregroundColor(.purple)
-                TextField("E-mail", text: $viewModel.singInAccount.email)
-                    .fontWeight(.medium)
-            }
-            .padding()
-            .overlay {
-                RoundedRectangle(cornerRadius: 8, style: .continuous)
-                    .stroke(Color(UIColor.systemGray4), lineWidth: 2)
-            }
-            .padding([.horizontal])
-            
-            
-            HStack{
-                Image(systemName: "lock.fill")
-                    .foregroundColor(.purple)
-                SecureField("Senha", text: $viewModel.singInAccount.password)
-            }
-            .padding()
-            .overlay {
-                RoundedRectangle(cornerRadius: 8, style: .continuous)
-                    .stroke(Color(UIColor.systemGray4), lineWidth: 2)
-            }
-            .padding()
-            
-            Button {
-                Task {
-                    await viewModel.placeOrder()
+            if  viewModel.userIsAuthenticated {
+                RegisteredChildren()
+            } else {
+                Image("happy.family")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 300, maxHeight: 300)
+                    .padding([.bottom])
+                
+                HStack{
+                    Image(systemName: "envelope.fill")
+                        .foregroundColor(.purple)
+                    TextField("E-mail", text: $viewModel.singInAccount.email)
+                        .fontWeight(.medium)
                 }
-            } label: {
-                Text("Entrar")
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .maximum(300, 300))
-                    .padding(4)
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .stroke(Color(UIColor.systemGray4), lineWidth: 2)
+                }
+                .padding([.horizontal])
+                
+                
+                HStack{
+                    Image(systemName: "lock.fill")
+                        .foregroundColor(.purple)
+                    SecureField("Senha", text: $viewModel.singInAccount.password)
+                }
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .stroke(Color(UIColor.systemGray4), lineWidth: 2)
+                }
+                .padding()
+                
+                Button {
+                    Task {
+                        await viewModel.placeOrder()
+                    }
+                } label: {
+                    Text("Entrar")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .maximum(300, 300))
+                        .padding(4)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+                .padding(.bottom)
             }
-            .buttonStyle(.borderedProminent)
-            .tint(.purple)
-            .padding(.bottom)
             
-//            NavigationLink(value: navi) {
-//                if let hasValue = navi {
-//                    RegisteredChildren()
-//                }
-//
-//            }
         }
         .navigationTitle("Login")
         .navigationBarTitleDisplayMode(.inline)
