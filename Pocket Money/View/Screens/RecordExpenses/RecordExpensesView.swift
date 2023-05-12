8a0782e04b52fe1c986df5ea5f582c418f8b4805
Dyan <lumbar_plaice0i@icloud.com>
2023-05-12 17:15:41 -0300

    feat: field description and money

--- Pocket Money/View/Screens/RecordExpenses/RecordExpensesView.swift
+++ Pocket Money/View/Screens/RecordExpenses/RecordExpensesView.swift
@@ -7,31 +7,69 @@
 
 import SwiftUI
 
-struct RecordExpenses: View {
-    @State var amount = String()
-    @State var description = String()
+struct Movimentation {
+    let id: String
+    let description: String
+    let value: Double
+    let type: String
+    let kidId: String
+    let createdAt: String
+    let updatedAt: String
+}
 
+class TextBindingManager: ObservableObject {
+    @Published var text = "" {
+        didSet {
+            if text.count > characterLimit && oldValue.count <= characterLimit {
+                text = oldValue
+            }
+        }
+    }
+    let characterLimit: Int
+
+    init(limit: Int = 5){
+        characterLimit = limit
+    }
+}
+
+struct RecordExpenses: View {
+    @ObservedObject var description = TextBindingManager(limit: 137)
+    @State private var isSubtitleHidden = false
+    @State private var value = 0
+    
+    private var numberFormatter: NumberFormatter
+    
+    init(numberFormatter: NumberFormatter = NumberFormatter()) {
+        self.numberFormatter = numberFormatter
+        self.numberFormatter.locale = Locale(identifier: "pt_BR")
+        self.numberFormatter.numberStyle = .currency
+        self.numberFormatter.maximumFractionDigits = 2
+    }
+
+    
     var body: some View {
-        NavigationView {
+        NavigationStack{
             GeometryReader { geometry in
                 ScrollView(.vertical) {
                     VStack{
                         HStack{
                             Image(systemName: "dollarsign")
                                 .foregroundColor(.purple)
-                            TextField("Valor", text: $amount)
+                            CurrencyTextField(numberFormatter: numberFormatter, value: $value)
+                                          .frame(height: 4)
+                                      Rectangle()
+                                          .frame(width: 0, height: 10)
+                        }
+                        .textFieldBorderIcon()
+                        HStack{
+                            Image(systemName: "note.text")
+                                .foregroundColor(.purple)
+                            TextField("Descrição", text: $description.text, axis: .vertical)
+                                .lineLimit(4)
                                 .fontWeight(.medium)
                         }
                         .textFieldBorderIcon()
                         
-                        HStack{
-                            Image(systemName: "note.text")
-                                .foregroundColor(.purple)
-                            TextField("Descrição", text: $description)
-                                .fontWeight(.medium)
-                        }
-                        .textFieldBorderIcon()
-                    
                         Button {
                             Task{
                                 //await viewModel.createChild()
@@ -52,17 +90,7 @@ struct RecordExpenses: View {
                     .frame(minHeight: geometry.size.height)
                 }
                 .navigationTitle("Registrar Gasto")
-                .navigationBarTitleDisplayMode(.large)
-//                .toolbar{
-//                    ToolbarItem(placement: .navigationBarLeading) {
-//                        Button {
-//                            //viewModel.showSheetToggle()
-//                        } label: {
-//                            Image(systemName: "chevron.backward")
-//                                .fontWeight(.bold)
-//                        }
-//                    }
-//                }
+                .navigationBarTitleDisplayMode(.inline)
             }
         }
     }
@@ -73,5 +101,101 @@ struct RecordExpenses_Previews: PreviewProvider {
         NavigationStack{
             RecordExpenses()
         }
+    
     }
+}
+
+struct CurrencyTextField: UIViewRepresentable {
+    
+    typealias UIViewType = CurrencyUITextField
+    
+    let numberFormatter: NumberFormatter
+    let currencyField: CurrencyUITextField
+    
+    init(numberFormatter: NumberFormatter, value: Binding<Int>) {
+        self.numberFormatter = numberFormatter
+        currencyField = CurrencyUITextField(formatter: numberFormatter, value: value)
+    }
+    
+    func makeUIView(context: Context) -> CurrencyUITextField {
+        return currencyField
+    }
+    
+    func updateUIView(_ uiView: CurrencyUITextField, context: Context) { }
+}
+
+
+
+import UIKit
+class CurrencyUITextField: UITextField {
+    
+    @Binding private var value: Int
+    private let formatter: NumberFormatter
+    
+    init(formatter: NumberFormatter, value: Binding<Int>) {
+        self.formatter = formatter
+        self._value = value
+        super.init(frame: .zero)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+        addTarget(self, action: #selector(resetSelection), for: .allTouchEvents)
+        keyboardType = .numberPad
+        textAlignment = .right
+        sendActions(for: .editingChanged)
+    }
+    
+    override func deleteBackward() {
+        text = textValue.digits.dropLast().string
+        sendActions(for: .editingChanged)
+    }
+    
+    private func setupViews() {
+        tintColor = .clear
+        font = .systemFont(ofSize: 24, weight: .regular)
+    }
+    
+    @objc private func editingChanged() {
+        text = currency(from: decimal)
+        resetSelection()
+        value = Int(doubleValue * 100)
+    }
+    
+    @objc private func resetSelection() {
+        selectedTextRange = textRange(from: endOfDocument, to: endOfDocument)
+    }
+    
+    private var textValue: String {
+        return text ?? ""
+    }
+
+    private var doubleValue: Double {
+      return (decimal as NSDecimalNumber).doubleValue
+    }
+
+    private var decimal: Decimal {
+      return textValue.decimal / pow(10, formatter.maximumFractionDigits)
+    }
+    
+    private func currency(from decimal: Decimal) -> String {
+        return formatter.string(for: decimal) ?? ""
+    }
+}
+
+extension StringProtocol where Self: RangeReplaceableCollection {
+    var digits: Self { filter (\.isWholeNumber) }
+}
+
+extension String {
+    var decimal: Decimal { Decimal(string: digits) ?? 0 }
+}
+
+extension LosslessStringConvertible {
+    var string: String { .init(self) }
 }
